2e3442c93f6ef139c67a62ddfb5e4b6a86f0a6bb
Valeriia Savenko <vasavenk@microsoft.com>
2018-06-14 12:45:32 +1000

    another com

--- env.js
+++
@@ -1,5 +0,0 @@
-env = {
-    HOSTNAME: 'DESKTOP-E73I89M',
-    STORAGEACCOUNT_NAME: 'storageqna',
-    STORAGEACCOUNT_KEY: 'JMSkY+o7WttRGkiO2MOyBmqjbWBan7zTOObkQp+wbQVVjwgnmTBJw2U7e9jzfhuibibwgsiQh6vhMOrq4uMhtA=='
-}
